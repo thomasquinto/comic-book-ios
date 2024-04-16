f44e94a7b291b946b36bad488d9eb3da3d32efa9
thomasquinto <thomasq@gmail.com>
2024-04-16 09:40:07 -0700

    Initial Commit

---
+++ ComicBook/ComicBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicBookApp.swift
+//  ComicBook
+//
+//  Created by Thomas Quinto on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ComicBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
